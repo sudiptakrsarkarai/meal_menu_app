adc30447b9d35580298210580c6d0294022aacfb
sudiptakrsarkarai <battledog049@gmail.com>
2024-03-08 14:29:29 +0530

    Flutter Meal Menu App

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/models/category.dart';
+// import 'package:meals/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +41,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -7,40 +8,34 @@ class MealsScreen extends StatelessWidget {
   final List<Meal> meals;
 
   Widget build(context) {
-    Widget content = ListView.builder(
-      itemCount: meals.length,
-      itemBuilder: (context, index) => Text(
-        meals[index].title,
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text("Uh oh ... nothing here!"),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            "Try Selecting a different category!",
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          )
+        ],
       ),
     );
-    if (meals.isEmpty) {
-      content = Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text("Uh oh ... nothing here!"),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              "Try Selecting a different category!",
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            )
-          ],
-        ),
-      );
 
-      if (meals.isNotEmpty) {
-        content = ListView.builder(
-            itemCount: meals.length,
-            itemBuilder: (context, index) => Text(meals[index].title));
-      }
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (context, index) => MealItem(meal: meals[index]),
+      );
     }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: Text(title),
       ),
       body: content,
     );
