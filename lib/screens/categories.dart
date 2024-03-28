7de6248b4ab3ca22b562eb9a24a44c74237b5eca
sudiptakrsarkarai <battledog049@gmail.com>
2024-03-28 20:39:41 +0530

    Adding Filter Option

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals/data/dummy_data.dart';
-import 'package:meals/screens/meals.dart';
-import 'package:meals/widgets/category_grid_item.dart';
-import 'package:meals/models/category.dart';
 import 'package:meals/models/meal.dart';
-// import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key, required this.onToggleFavourite});
-  final void Function(Meal meal) onToggleFavourite;
-  void _selectCategory(context, Category category) {
-    final filteredMeals = dummyMeals
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+    required this.availableMeals,
+  });
+
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
-    Navigator.push(
-      context,
+
+    Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
-          onToggleFavourite: onToggleFavourite,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
-    );
+    ); // Navigator.push(context, route)
   }
 
   @override
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,43 +1,51 @@
 import 'package:flutter/material.dart';
+
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen(
-      {super.key,
-      this.title,
-      required this.meals,
-      required this.onToggleFavourite});
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+    required this.onToggleFavorite,
+  });
+
   final String? title;
   final List<Meal> meals;
-  final void Function(Meal meal) onToggleFavourite;
+  final void Function(Meal meal) onToggleFavorite;
+
   void selectMeal(BuildContext context, Meal meal) {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => MealDetailsScreen(
+        builder: (ctx) => MealDetailsScreen(
           meal: meal,
-          onToggleFavourite: onToggleFavourite,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
   }
 
+  @override
   Widget build(BuildContext context) {
     Widget content = Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Text("Uh oh ... nothing here!"),
-          const SizedBox(
-            height: 16,
-          ),
           Text(
-            "Try Selecting a different category!",
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category!',
             style: Theme.of(context).textTheme.bodyLarge!.copyWith(
                   color: Theme.of(context).colorScheme.onBackground,
                 ),
-          )
+          ),
         ],
       ),
     );
@@ -45,7 +53,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (context, index) => MealItem(
+        itemBuilder: (ctx, index) => MealItem(
           meal: meals[index],
           onSelectMeal: (meal) {
             selectMeal(context, meal);
@@ -53,9 +61,11 @@ class MealsScreen extends StatelessWidget {
         ),
       );
     }
+
     if (title == null) {
       return content;
     }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title!),
